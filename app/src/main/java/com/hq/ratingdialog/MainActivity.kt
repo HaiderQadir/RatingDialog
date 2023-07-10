246107fa4c7d72d74dfefd8240411f1ac48de2a8
Haider Qadir <48542399+HaiderQadir@users.noreply.github.com>
2023-07-10 12:51:37 +0500

    UI Updated

--- app/src/main/java/com/hq/ratingdialog/MainActivity.kt
+++ app/src/main/java/com/hq/ratingdialog/MainActivity.kt
@@ -8,19 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var fmL = supportFragmentManager
-        var io: RatingDialog = RatingDialog(
-            RatingModel(
-                " Your Opinion Matters! ",
-                "We value your feedback! Help us improve our dialog box feature with your valuable input.",
-                " ",
-                " ",
-                false,
-                2
-            )
-        )
-        io.show(fmL, "")
-
     }
 }
